e5bd29ea325e5c2bffaa0a661fdde35c26675c6f
versedpro <ultraj0330@gmail.com>
2022-07-07 00:37:05 +0800

    style: Implement tailwind style and use Image component of next

--- components/Card/FormCard.tsx
+++ components/Card/FormCard.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import classNames from 'classnames';
 import styles from './Card.module.css';
+import Image from 'next/image';
+import Liked from '../../public/image/liked.svg';
+import UnLiked from '../../public/image/unliked.svg';
+import Verified from '../../public/image/verified.svg';
 
 const FormCard = ({
   title,
@@ -21,15 +25,19 @@ const FormCard = ({
     <div className={classNames([styles.wrapper, styles.wrapperAnime])}>
       <div className={styles.header}>
         <div className={styles.imageWrapper}>
-          <img src={image} className={styles.image} alt="card background" />
+          <Image src={image} className={styles.image} layout="fill" alt="card background" />
         </div>
         <div className={styles.badgeWrapper}>
-          <div className={classNames([styles.dangerBadge, styles.badgeAnime])}>
-            {like ? <img src="/image/liked.svg" /> : <img src="/image/unliked.svg" />}
+          <div className={`${classNames([styles.dangerBadge, styles.badgeAnime])} w-[22.5px] h-[21px]`}>
+            {like ? (
+              <Image src={Liked} layout="fill" alt="liked" />
+            ) : (
+              <Image src={UnLiked} layout="fill" alt="unliked" />
+            )}
           </div>
           <div className={styles.primaryBadge}>
             <span className={styles.counter}>{price}</span>
-            <img src={`/image/token/${currency}.svg`} />
+            <Image src={`/image/token/${currency}.svg`} width={18} height={18} layout="fixed" />
           </div>
         </div>
       </div>
@@ -37,7 +45,7 @@ const FormCard = ({
         <div className={styles.textTitle}>{title}</div>
         <div className={styles.region}>
           <p className={styles.textRegion}>{region}</p>
-          <img src="/image/verified.svg" alt="verify notification" />
+          <Image src={Verified} width={18} height={18} layout="fixed" alt="verify notification" />
         </div>
       </div>
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ const Home: NextPage = () => {
 
   // Connect your marketplace smart contract here (replace this address)
   const marketplace = useMarketplace(
-    '0x277C0FB19FeD09c785448B8d3a80a78e7A9B8952' // Your marketplace contract address here
+    '' // Your marketplace contract address here
   );
 
   const { data: listings, isLoading: loadingListings } = useActiveListings(marketplace);
