05f87be7d816c11b09a170a02ab036e906c2e8ad
versedpro <ultraj0330@gmail.com>
2022-07-14 11:35:51 +0800

    refactor: Update result parts

--- data/index.ts
+++ data/index.ts
@@ -1,3 +1,4 @@
 export { default as cards } from './cards';
 export { activities as Activities } from './activityItems';
 export { default as Comments } from './comments';
+export { default as SearchResults } from './searchResults';
